ce42674fa148a3680aaa330449fdd23a3d2c8649
AbdouRoumi <a_roumi@estin.dz>
2024-09-20 23:08:47 +0100

    Creating full target path

--- RemoteThreadHijacking/RemoteThreadHijacking.cpp
+++ RemoteThreadHijacking/RemoteThreadHijacking.cpp
@@ -1,10 +1,10 @@
 #include "Functions.h"
 
 
-BOOL CreateSuspendedProcess() {
+BOOL CreateSuspendedProcess(IN LPCSTR ProcessName) {
 
 	CHAR WnDr[MAX_PATH];
-
+	CHAR lpPath[MAX_PATH * 2];
 
 	if (!GetEnvironmentVariableA("WINDIR", WnDr, MAX_PATH)) {
 		warning("Get Environment Variable failed error : 0x%lx", GetLastError());
@@ -14,7 +14,10 @@ BOOL CreateSuspendedProcess() {
 
 	okay("We got the env var");
 	//Creating full target
-	if()
+	sprintf(lpPath, "%s\\System32\\%s", WnDr, ProcessName);
+	info("Running : %s", lpPath);
+
+
 
 	if(!CreateProcessA(NULL,){}
 }
